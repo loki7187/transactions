52250bab641d2ce828223cb895a52cb97bf7c0d9
loki <loki7187@gmail.com>
2022-07-20 22:34:11 +0300

    init spring project

---
+++ src/test/java/ru/loki7187/microsrv/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.loki7187.microsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
