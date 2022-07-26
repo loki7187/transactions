dbfa932d369d5a4b020de41f1d69545fccadc240
Loki7187 <loki7187@gmail.com>
2022-07-26 18:32:16 +0300

    restore project structure

--- app/src/main/java/ru/loki7187/app/dao/CardRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.loki7187.app.dao;
-
-
-import org.springframework.data.repository.CrudRepository;
-import ru.loki7187.app.entity.CardEntity;
-
-public interface CardRepo extends CrudRepository<CardEntity, Long> {
-}
--- app/src/main/java/ru/loki7187/app/globalconfig/Constants.java
+++
@@ -1,9 +0,0 @@
-package ru.loki7187.app.globalconfig;
-
-public class Constants {
-
-    public static final String success = "success";
-    public static final String notEnoughtMoney = "notEnoughtMoney";
-    public static final String cardNotFound = "cardNotFound";
-    public static final String err = "err";
-}
